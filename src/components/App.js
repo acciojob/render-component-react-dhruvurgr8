f69a5468e51c6370c5884ee2aa5812e3042e2283
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-12 06:40:32 +0000

    assigment2

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,11 @@
 import React, {Component, useState} from "react";
 import '../styles/App.css';
-
+import Test from "./Test";
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <Test />
+    </div>
   )
 }
 
---
+++ src/components/Test.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Test = () =>{
+    return <p>I am learning React. My life is getting better.</p> ;
+}
+export default Test;
